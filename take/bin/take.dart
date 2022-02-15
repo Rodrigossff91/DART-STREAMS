d0e997f1b2feb30faed089f199a92dbed2b06a01
Rodrigo Ferreira <rodrigo@treeinova.com>
2022-02-15 14:31:53 -0300

    Streams Take

--- take/bin/take.dart
+++ take/bin/take.dart
@@ -1,8 +1,9 @@
 Future<void> main(List<String> arguments) async {
   final interval = Duration(seconds: 2);
   var stream = Stream<int>.periodic(interval, callback);
+
   stream = stream.takeWhile((int numero) => numero < 10);
-  // stream = stream.take(5);
+  stream = stream.take(5);
 
   await for (var i in stream) {
     print(i);
